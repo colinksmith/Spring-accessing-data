a40da79e91522da07db454d85fa2573b4a40f9f2
York MacBook 079 <yorkmacbook079@Yorks-MacBook-Pro.local>
2024-02-13 15:01:39 -0500

    initial commit

---
+++ src/main/java/com/example/assessingdata/AssessingDataApplication.java
@@ -0,0 +1,13 @@
+package com.example.assessingdata;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AssessingDataApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AssessingDataApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/assessingdata/AssessingDataApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.assessingdata;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AssessingDataApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
